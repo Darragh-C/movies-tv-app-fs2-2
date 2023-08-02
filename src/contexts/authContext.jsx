0126661f618995c6e77590d6252e806f102626ac
Darragh Conneely <darraghc23@gmail.com>
2023-08-02 20:17:29 +0100

    added basic user credential validation

--- src/contexts/authContext.jsx
+++ src/contexts/authContext.jsx
@@ -1,13 +1,22 @@
 import React, { useState, useEffect, createContext } from "react";
+import testUserCredentials from "../dataStore/testUserCredentials.json";
 
 export const AuthContext = createContext(null);
 
 const AuthContextProvider = ({ props }) => {
   const [user, setUser] = useState({ username: null, password: null });
 
-  const authenticate = (username, password) => {
+  const authenticate = (email, password) => {
     // Validate user credentials here
-    setUser({ username, password });
+    const userCredentials = testUserCredentials.filter((user) => user.email === email);
+    const validUser = userCredentials && userCredentials.password === password;
+  
+    if (validUser) {
+      setUser({ email, password });
+    } else {
+      console.log("invalid credentials");
+    }
+    
   };
 
   const isAuthenticated = user.username === null ? false : true;
